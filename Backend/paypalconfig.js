9ae971daa6473bcdd655e3a88fdf7f45cb4bc277
sarammalS <sarammalbca2003@gmail.com>
2025-01-28 19:35:26 +0530

    Update paypalconfig.js

--- Backend/paypalconfig.js
+++ Backend/paypalconfig.js
@@ -2,8 +2,8 @@ const paypal = require('paypal-rest-sdk');
 
 paypal.configure({
     'mode': 'sandbox', // Change to 'live' for production
-    'client_id': 'AWuP9le6W2blEu1U1cpe72b-jmfTUhjkIZRrY0ROBdaxcMx853BvRL8OEGdme0ZwDMJ4s_aOiDhSTMol',
-    'client_secret': 'ECrUJfUX_wR57txuFaqVu-yKShDnKz8F_POIUBIuJEY0RrlCS4NHgAjbMHTeRg1Ql-pF1jr_CXVC3vgS'
+    'client_id': '',
+    'client_secret': '';
 });
 
 module.exports = paypal;
